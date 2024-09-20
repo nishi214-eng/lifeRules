4a53cca4a64a0cd72e0b9643382ce3749dae5301
nishi214-eng <89783904+nishi214-eng@users.noreply.github.com>
2024-09-20 22:13:46 +0900

    [add]プッシュ通知機能を追加 #12
    expoのusageからプッシュ通知機能のテンプレートを導入した

--- feature/notification.tsx
+++ feature/notification.tsx
@@ -1,16 +1,8 @@
-//import { Image, StyleSheet, Platform } from 'react-native';
-import { Platform } from 'react-native';
-//import { HelloWave } from '@/components/HelloWave';
-//import ParallaxScrollView from '@/components/ParallaxScrollView';
-//import { ThemedText } from '@/components/ThemedText';
-//import { ThemedView } from '@/components/ThemedView';
 import { useState, useEffect, useRef } from 'react';
-import { Text, View, Button } from 'react-native';
-//import * as Device from 'expo-device';
+import { Text, View, Button, Platform } from 'react-native';
+import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
-//import Constants from 'expo-constants';
-import { schedulePushNotification } from '../notifications';
-import { registerForPushNotificationsAsync } from '../notifications';
+import Constants from 'expo-constants';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -70,11 +62,70 @@ export default function App() {
         <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
       </View>
       <Button
-        title="通知を送信"
+        title="Press to schedule a notification"
         onPress={async () => {
           await schedulePushNotification();
         }}
       />
     </View>
   );
+}
+
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got mail! 📬",
+      body: 'Here is the notification body',
+      data: { data: 'goes here', test: { test1: 'more data' } },
+    },
+    trigger: { seconds: 2 },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    // Learn more about projectId:
+    // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+    // EAS projectId is used here.
+    try {
+      const projectId =
+        Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+      if (!projectId) {
+        throw new Error('Project ID not found');
+      }
+      token = (
+        await Notifications.getExpoPushTokenAsync({
+          projectId,
+        })
+      ).data;
+      console.log(token);
+    } catch (e) {
+      token = `${e}`;
+    }
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  return token;
 }
