dedc4612ecab2d072d75aef073c5e0b08edb2a8d
ImagawaKosuke <kosuke8de@gmail.com>
2024-09-22 08:03:34 +0900

    kosuke/makeprofile

--- app/pages/profile.tsx
+++ app/pages/profile.tsx
@@ -1,5 +1,11 @@
 import { useState } from 'react';
-import { auth } from '../(tabs)/firebaseConfig';  // firebase.ts から auth をインポート
+import { auth } from "@/app/(tabs)/firebaseConfig";
+import { db } from "@/app/(tabs)/firebaseConfig";
+import { doc, getDoc, setDoc, addDoc, collection } from 'firebase/firestore';
+
+import { getAuth, getIdTokenResult } from 'firebase/auth';
+
+
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { View, TextInput, Button, Text, StyleSheet, Image } from 'react-native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
@@ -16,6 +22,26 @@ export default function ProfileScreen({ navigation }: Props) {
     const [password, setPassword] = useState<string>('');
     const [errorMessage, setErrorMessage] = useState<string | null>(null);
     const [user, setUser] = useState<any | null>(null);
+
+    const uid = auth.currentUser?.uid;
+    if (!uid) {
+        console.error('User is not authenticated');
+        return;
+    }
+    const userDocRef = doc(db, 'users', uid);
+    getDoc(userDocRef)
+        .then((doc) => {
+            if (doc.exists()) {
+                const userData = doc.data();
+                console.log('User data:', userData);
+            } else {
+                console.log('No such document!');
+            }
+        })
+        .catch((error) => {
+            console.error('Error getting document: ', error);
+        });
+
 
     const handleLogin = () => {
         // Firebase Authenticationを使ってユーザーをログイン
@@ -36,6 +62,11 @@ export default function ProfileScreen({ navigation }: Props) {
 
     return (
         <View style={styles.container}>
+            <Image style={styles.usericon}
+                source={require('@/assets/images/partial-react-logo.png')}
+            />
+            <View style={{ height: 20 }}></View>
+            <Text>{uid}</Text>
             {user ? (
                 <Text>{user.email} でログインしています</Text>
             ) : (
@@ -63,7 +94,6 @@ export default function ProfileScreen({ navigation }: Props) {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         justifyContent: 'center',
         padding: 20,
     },
@@ -86,9 +116,18 @@ const styles = StyleSheet.create({
         justifyContent: 'space-around',
     },
     profileimage: {
+        marginTop: 100,
         width: 100,
         height: 100,
         borderColor: 'red',
         borderRadius: 30,
-    }
+    },
+    usericon: {
+        marginTop: 50,
+        width: 150,
+        height: 150,
+        backgroundColor: '#eeeeee',
+        marginLeft: "30%",
+        borderRadius: 100,
+    },
 });
